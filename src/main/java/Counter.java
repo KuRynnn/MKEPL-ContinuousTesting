c8a72a36e8a6e72df777deb177ddbf74b65ea28f
KuRynnn <fahryan107@gmail.com>
2024-05-16 18:00:40 +0700

    First Init

---
+++ src/main/java/Counter.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Counter {
+    private int count;
+	
+	public Counter() {
+		this.reset();
+	}
+		
+	public void reset() {
+		count = 0;
+	}
+	
+	public void increment() {
+		count++;
+	}
+	
+	public void decrement() {
+		count--;
+	}
+	
+	public int getCount() {
+		return count;
+	}
+}
---
+++ src/test/java/CounterTest.java
@@ -0,0 +1,47 @@
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class CounterTest {
+    @Test
+	public void testReset() {
+		Counter testCounter = new Counter();
+		
+		for(int i = 0; i <10; i++) {
+			testCounter.increment();
+		}
+		
+		testCounter.reset();
+		
+		assertEquals(testCounter.getCount(), 0);
+	}
+
+	@Test
+	public void testIncrement() {
+		Counter testCounter = new Counter();
+		
+		for(int i = 1; i <10; i++) {
+			testCounter.increment();
+			assertEquals(testCounter.getCount(), i);
+		}
+	}
+
+	@Test
+	public void testDecrement() {
+		Counter testCounter = new Counter();
+		
+		for(int i = 1; i <10; i++) {
+			testCounter.decrement();
+			assertEquals(testCounter.getCount(), i * -1);
+		}
+	}
+}
